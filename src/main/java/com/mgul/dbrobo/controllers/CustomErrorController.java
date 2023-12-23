1e43c68ee54390f97529f906c103bbacf6b1d341
anton <anton.chugunow@yandex.ru>
2023-12-23 12:12:02 +0300

    update CustomErrorController

--- src/main/java/com/mgul/dbrobo/controllers/CustomErrorController.java
+++ src/main/java/com/mgul/dbrobo/controllers/CustomErrorController.java
@@ -1,6 +1,7 @@
 package com.mgul.dbrobo.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -18,8 +19,14 @@ public class CustomErrorController implements ErrorController {
     @ResponseBody
     public Map<String, String> handleError(HttpServletRequest httpServletRequest) {
         LinkedHashMap<String, String> map = new LinkedHashMap<>();
-        map.put("path", httpServletRequest.getRequestURI());
+        map.put("error exception", (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        map.put("error exception type", (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        map.put("error message", (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        map.put("error request uri", (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        map.put("error servlet name", (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+//        map.put("error status code", (Integer) httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
         map.put("method", httpServletRequest.getMethod());
+        map.put("forward request uri", (String) httpServletRequest.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI));
         return map;
     }
 
